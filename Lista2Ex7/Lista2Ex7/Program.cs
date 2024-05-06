0fc4ba95058c75a6da6b0715c6e103fbabaf9e39
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-05-06 20:41:51 -0300

    Add files via upload

---
+++ Lista2Ex7/Lista2Ex7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.Write("Digite o valor do lado 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado 2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado 3: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a + b > c)
+                if (a + c > b)
+                    if (b + c > a)
+                        if (a == b)
+                            if (a == c)
+                                Console.WriteLine("Equilátero");
+                            else
+                                Console.WriteLine("Isósceles");
+                        else
+                            if (b == c)
+                                Console.WriteLine("Isósceles");
+                            else
+                               if (a == c)
+                                  Console.WriteLine("Isósceles");
+                               else
+                                  Console.WriteLine("Escaleno");
+                        else
+                           Console.WriteLine("Não forma triângulo");
+                     else
+                        Console.WriteLine("Não forma triângulo");
+                else
+                   Console.WriteLine("Não forma triângulo");
+
+        }
+    }
+}
---
+++ Lista2Ex7b/Lista2Ex7b/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex7b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.Write("Digite o valor do lado 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado 2: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do lado 3: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+                if (a == b && a == c)
+                    Console.WriteLine("Equilatero");
+                else
+                    if (a == b || a == c || b == c)
+                    Console.WriteLine("Isósceles");
+                else
+                    Console.WriteLine("Escaleno");
+            else
+                Console.WriteLine("Não forma triângulo");
+        }
+    }
+}
